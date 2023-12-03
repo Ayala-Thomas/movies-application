b55bcd990bf995dd6be3c4a7ffe4a0e1b588d99e
Marquis Thomas <MarquisThomas6991@gmail.com>
2023-12-03 16:06:01 -0600

    sundaysUpdate1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 import {grabMovies, grabMovie, deleteMovie, postMovie, patchMovie} from "./api/movies-api.js";
 
+const draggablecontainer = document.querySelector(".dragglecontainer")
+const search = document.querySelector("#search")
+
+
+
+
+    let isDragging = false;
+    let offsetX, offsetY;
+draggablecontainer.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offsetX = e.clientX - draggablecontainer.getBoundingClientRect().left;
+    offsetY = e.clientY - draggablecontainer.getBoundingClientRect().top;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+
+    const x = e.clientX - offsetX;
+    const y = e.clientY - offsetY;
+
+    draggablecontainer.style.left = `${x}px`;
+    draggablecontainer.style.top = `${y}px`;
+});
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
 
 (async ()=>{
     const getMovies = await grabMovies()
@@ -7,4 +33,7 @@ import {grabMovies, grabMovie, deleteMovie, postMovie, patchMovie} from "./api/m
     const getMovie = await grabMovie(2)
     console.log(getMovie)
 
+    search.addEventListener("mousedown", ()=>{
+        draggablecontainer.classList.toggle("display")
+    })
 })();
